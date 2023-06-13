2300c2e8f0e044ec37a5b19c8f43455c5ba75c70
Dan Phuong <112434443+dan-phuong@users.noreply.github.com>
2023-06-13 12:53:56 -0600

    script to run coremark

---
+++ run.py
@@ -0,0 +1,20 @@
+# Run install.py before hand
+import subprocess
+import os
+
+pwd = 'coremark'
+
+def run_cmd(command):
+    p1 = subprocess.call([command], cwd=pwd)
+    results('coremark/run1.log')
+    results('coremark/run2.log')
+
+def results(runs):
+    print("=======================================")
+    print("Results:")
+    file = open(runs, 'r')
+    for line in file:
+        print(line)
+
+run_cmd('make')
+
